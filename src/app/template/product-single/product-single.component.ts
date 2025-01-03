d2d86e964742656b1f076fefc166925d53e7cf0b
Euller_Gabriel <eullergoliveira@gmail.com>
2025-01-03 19:38:38 +0000

    Adicionei lógica que mostra o nome do produto no titulo da página product single

--- src/app/template/product-single/product-single.component.ts
+++ src/app/template/product-single/product-single.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SelectProductService } from '../../services/select-product.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-product-single',
@@ -13,12 +14,18 @@ export class ProductSingleComponent implements OnInit {
 
   product: any;
 
-  constructor(private selectedProduct: SelectProductService, private router: Router) { }
+  constructor(
+    private selectedProduct: SelectProductService,
+    private router: Router,
+    private titleService: Title
+  ) {}
   
   ngOnInit(): void {
     this.product = this.selectedProduct.getProduct()
 
-    if (!this.product) {
+    if (this.product) {
+      this.titleService.setTitle(this.product.title)
+    } else {
       this.router.navigate(['/home-page'])
     }
   }
