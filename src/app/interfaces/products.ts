c08aa99bbbddf4a6a2898fad13008bbfd2c71e2d
Euller_Gabriel <eullergoliveira@gmail.com>
2025-01-06 15:49:36 +0000

    Conectei projeto à API do Python, home page ja lista os itens da base de dados.

--- src/app/interfaces/products.ts
+++ src/app/interfaces/products.ts
@@ -1,9 +1,8 @@
 export interface Products {
-    albumId?: number,
     id?: number,
-    title: string,
-    url: string,
-    thumbnailUrl: string,
+    product_name: string,
+    product_price: number,
+    product_image: string,
     quantity?: number
 
 }
