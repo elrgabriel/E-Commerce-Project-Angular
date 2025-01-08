6076da2513c68bcc02b042e39b1eb399c046a4ca
Euller_Gabriel <eullergoliveira@gmail.com>
2025-01-08 20:34:53 +0000

    FIX: Correção de merge feito de forma errda

--- src/app/interfaces/products.ts
+++ src/app/interfaces/products.ts
@@ -1,3 +1,9 @@
+export interface Products {
+    id?: number,
+    product_name: string,
+    product_price: number,
+    product_image: string,
     quantity?: number,
     totalPrice?: number
 
+}
